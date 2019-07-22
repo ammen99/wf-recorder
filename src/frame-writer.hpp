cfffb0a5859c191e00ce8abfe0fa32fbd12db5b0
James Edwards-Jones <jedwardsjones@gitlab.com>
2019-07-22 08:17:28 +0100

    Add --pixel-format option
    Allows forcing yuv420p, needed for screensharing with zoom

--- src/frame-writer.hpp
+++ src/frame-writer.hpp
@@ -43,6 +43,7 @@ struct FrameWriterParams
 
     std::string codec;
     std::string muxer;
+    std::string pix_fmt;
     std::string hw_device; // used only if codec contains vaapi
     std::map<std::string, std::string> codec_options;
 
@@ -68,6 +69,7 @@ class FrameWriter
     AVBufferRef *hw_device_context = NULL;
     AVBufferRef *hw_frame_context = NULL;
 
+    AVPixelFormat lookup_pixel_format(std::string pix_fmt);
     AVPixelFormat choose_sw_format(AVCodec *codec);
     AVPixelFormat get_input_format();
     void init_hw_accel();
