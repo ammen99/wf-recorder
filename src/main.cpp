c443ed6e9c2471f03db43bf0de6ef5030b62aef3
happysmash27 <happysmash27@vmail.me>
2020-07-14 22:34:35 -0700

    Reverted failed fix. Why do I get error: #endif sen #if ???

--- src/main.cpp
+++ src/main.cpp
@@ -557,13 +557,11 @@ With no FILE, start recording the current screen.)");
 #ifdef HAVE_OPENCL
     printf(R"(
 
-
   -e, --opencl              Use the -e[#] or --opencl[=#] in conjunction with -t or --force-yuv option
                             to use opencl for gpu accelerated conversion of data to yuv. # is one
                             of the devices listed when running without specifying #.)");
 #endif
     printf(R"(
-
 
   -t, --force-yuv           Use the -t or --force-yuv option to force conversion of the data to
                             yuv format, before sending it to the gpu.
