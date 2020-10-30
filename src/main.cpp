6d7ff6a7977d1c842467f86904ee8800b2fed284
Scott Moreau <oreaus@gmail.com>
2020-10-30 13:52:01 -0600

    Fix wrong command to discover audio input devices, advertised in --help

--- src/main.cpp
+++ src/main.cpp
@@ -529,7 +529,7 @@ Use Ctrl+C to stop.)");
                             [DEVICE] argument is optional.
                             In case you want to specify the pulseaudio device which will capture
                             the audio, you can run this command with the name of that device.
-                            You can find your device by running: pactl list sinks | grep Name)");
+                            You can find your device by running: pacmd list-sources | grep "name:")");
 #endif
     printf(R"(
 
