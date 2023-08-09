ef969acf6db67a59216c494129efabc986d165a4
Ilia Bozhinov <ammen99@gmail.com>
2023-08-09 11:21:21 +0200

    Revert "Use VP9+Opus on MKV for recordings"
    This reverts commit 163b5d328d53fab664fc8d9f00e40abe85eb81d1.

--- src/main.cpp
+++ src/main.cpp
@@ -810,7 +810,7 @@ Examples:)");
     printf(R"(
 
   - wf-recorder                         Records the video. Use Ctrl+C to stop recording.
-                                        The video file will be stored as recording.mkv in the
+                                        The video file will be stored as recording.mp4 in the
                                         current working directory.
 
   - wf-recorder -f <filename>.ext       Records the video. Use Ctrl+C to stop recording.
@@ -822,7 +822,7 @@ Examples:)");
   Video and Audio:
 
   - wf-recorder -a                      Records the video and audio. Use Ctrl+C to stop recording.
-                                        The video file will be stored as recording.mkv in the
+                                        The video file will be stored as recording.mp4 in the
                                         current working directory.
 
   - wf-recorder -a -f <filename>.ext    Records the video and audio. Use Ctrl+C to stop recording.
